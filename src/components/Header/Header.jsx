dc2f142e9e60a71f34d1ddcdeb81622ca87cb8c3
MdAbdullahIbnNoor <ab.89noor@gmail.com>
2023-09-28 01:29:08 +0600

    Google sign in pop up done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
-  return (
-    <div>
-        <h2 className='text-4xl text-center text-rose-600 font-bold my-7'>This is Header</h2>
-    </div>
-  )
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><Link to="/">Home</Link></li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header
